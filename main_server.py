cd62ba780f0c708b76abb0c9512ef01175668bce
m1zetz <riywer80@gmail.com>
2025-06-20 20:18:56 +0500

    flask1

---
+++ main_server.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from passlib.hash import bcrypt 
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+DATABASE_URL = "postgresql://neondb_owner:npg_FUqQJ7C3MKyN@ep-super-math-a91968rn-pooler.gwc.azure.neon.tech/neondb?sslmode=require"
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
+
+CORS(app)
+
+@app.route("/get_all_posts", methods=["GET"])
+def get_posts():
+     with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT post_text FROM posts")
+            rows = cur.fetchall()
+
+            posts = [row[0] for row in rows]
+            return jsonify({"posts": posts})
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.json
+    login = data["login"]
+    password = data["password"]
+
+    password_hash = bcrypt.hash(password)
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO users (login, password) VALUES (%s, %s)", (login, password_hash))
+            conn.commit()
+
+    return jsonify({"status": "registered"})
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    login = data.get("login")
+    password = data.get("password")
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT password FROM users WHERE login = %s", (login,))
+            result = cur.fetchone()
+
+            if result is None:
+                return jsonify({"status": "error", "message": "User not found"}), 404
+
+            password_hash = result[0]
+            if bcrypt.verify(password, password_hash):
+                return jsonify({"status": "success"})
+            else:
+                return jsonify({"status": "error", "message": "Wrong password"}), 401
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
